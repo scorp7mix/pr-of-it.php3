6a49d267a83e7e11e19b7effa1762b8a74d00fcc
scorp7mix <scorp7mix@gmail.com>
2016-04-06 21:08:44 +0300

    + db config

--- build/production/config.php
+++ build/production/config.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    'domain' => '{{domain}}'
+
+    'domain' => '{{domain}}',
+
+    'db' => [
+        'default' => [
+            'driver'   => 'mysql',
+            'host'     => '127.0.0.1',
+            'dbname'   => 'php3',
+            'user'     => 'root',
+            'password' => '{{db.password}}'
+        ]
+    ]
+
 ];
