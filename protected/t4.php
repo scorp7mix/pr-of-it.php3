3236193416517b4826f8f739d3435480ed148921
scorp7mix <scorp7mix@gmail.com>
2016-04-11 22:45:55 +0300

    ! t4 update

--- protected/t4.php
+++ protected/t4.php
@@ -7,4 +7,7 @@ require __DIR__ . '/autoload.php';
 
 \T4\Console\Application
     ::instance()
+    ->setConfig(
+        new \T4\Core\Config(ROOT_PATH_PROTECTED . '/config.php')
+    )
     ->run();
