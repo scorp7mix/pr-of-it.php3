08477209e8a1d630c068ad8d7ddfa00eaa5b3b78
scorp7mix <scorp7mix@gmail.com>
2016-04-11 20:59:35 +0300

    +add method get full name for user

---
+++ protected/Models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use T4\Orm\Model;
+
+/**
+ * Class User
+ * @package App\Models
+ *
+ * @property string $email
+ * @property string $password
+ * @property string $firstName
+ * @property string $secondName
+ * @property string $lastName
+ * @property string $fullName
+ */
+class User
+    extends Model
+{
+    protected static $schema = [
+        'columns' => [
+            'email'      => ['type' => 'string'],
+            'password'   => ['type' => 'string'],
+            'firstName'  => ['type' => 'string'],
+            'secondName' => ['type' => 'string'],
+            'lastName'   => ['type' => 'string'],
+        ],
+    ];
+
+    public function getFullName()
+    {
+        return
+            ( !empty($this->firstName) && !empty($this->lastName) )
+            ?
+                !empty($this->secondName)
+                ?
+                    implode(' ', [$this->firstName, $this->secondName, $this->lastName])
+                :
+                    implode(' ', [$this->firstName, $this->secondName])
+            :
+                $this->email
+            ;
+    }
+}
